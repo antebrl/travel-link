063a2b2a1df07102f67f84dd257c5f29ce8cc3bc
Ante Brähler <ante.braehler@stud.h-da.de>
2024-05-21 20:15:10 +0200

    feat: add bottomNavBar

---
+++ learning/braehler/lab3/chess_screen/lib/game_mode_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameModeTile extends StatelessWidget {
+  const GameModeTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- learning/braehler/lab3/chess_screen/lib/main.dart
+++ learning/braehler/lab3/chess_screen/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chess_screen/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,9 +17,9 @@ class MainApp extends StatelessWidget {
           leading:Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(width: 15),
+              const SizedBox(width: 15),
               SizedBox(height: 38,child: Image.asset('assets/appBar-top/profile.jpg')),
-              SizedBox(width: 15),
+              const SizedBox(width: 15),
               SizedBox(height: 30,child: Image.asset('assets/appBar-top/leagues.jpg')),
             ],
           ),
@@ -28,13 +29,47 @@ class MainApp extends StatelessWidget {
             child: Image.asset('assets/appBar-top/chessCom-logo.jpg')),
           actions: [
             SizedBox(height: 33,child: Image.asset('assets/appBar-top/friends-action.jpg')),
-            SizedBox(width: 20),
+            const SizedBox(width: 20),
           ],
           backgroundColor: const Color(0xFF252422),
         ),
-        
+        bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 12,
+          backgroundColor: const Color(0xFF252422),
+          showUnselectedLabels: true,
+          unselectedItemColor:const Color(0xFF7D7C7A),
+          selectedItemColor: const Color(0xFF739850),
+          items: [
+            BottomNavigationBarItem(
+              icon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/startseite.jpg')),
+              activeIcon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/selected-startseite.png')),
+              label: 'Startseite',
+              backgroundColor: const Color(0xFF252422),
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/taktikaufgaben.jpg')),
+              label: 'Taktikaufgaben',
+              backgroundColor: const Color(0xFF252422),
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/lernen.jpg')),
+              label: 'Lernen',
+              backgroundColor: const Color(0xFF252422),
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/anschauen.jpg')),
+              label: 'Anschauen',
+              backgroundColor: const Color(0xFF252422),
+            ),
+            BottomNavigationBarItem(
+              icon: SizedBox(height: 25, child: Image.asset('assets/navBar-bottom/mehr.jpg')),
+              label: 'Mehr',
+              backgroundColor: const Color(0xFF252422),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Homepage(),
         ),
       ),
     );
