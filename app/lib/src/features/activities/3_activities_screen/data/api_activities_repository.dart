3492c2fa2cb420e72f8e713b5df5ecee802e10b1
Ante Brähler <ante.braehler@stud.h-da.de>
2024-05-27 12:29:38 +0200

    feat: add activities repository

---
+++ app/lib/src/features/activities/3_activities_screen/data/api_activities_repository.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:travel_link/src/features/activities/3_activities_screen/domain/api_activity.dart';
+import 'package:travel_link/src/utils/constants/api_constants.dart';
+import 'package:travel_link/src/utils/logging/logger.dart';
+
+part 'api_activities_repository.g.dart';
+
+class ApiActivitiesRepository {
+
+  //TODO
+  // Future<Person> getActivityDetailsById({required int placeId}) async {
+    
+  // }
+
+  Future<List<ApiActivity>> getActivitiesInPlace({required String placeId}) async {
+    final String url =
+        '${CustomApiConstants.placesBaseURL}?categories=entertainment&filter=place:$placeId&apiKey=${CustomApiConstants.geoapifySecretKey}&limit=3';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final destinations =
+          json.decode(response.body)['features'] as List<dynamic>;
+
+      return destinations.map((activity) =>
+          ApiActivity.fromMap(activity['properties'] as Map<String, dynamic>),).toList();
+    } else {
+      logger.e(
+        'Failed to load and parse destination suggestions',
+        error: response.body,
+      );
+      return [];
+    }
+  }
+
+}
+
+@Riverpod(keepAlive: true)
+ApiActivitiesRepository apiActivitiesRepository(ApiActivitiesRepositoryRef ref) =>
+    ApiActivitiesRepository();
+
+@riverpod
+Future<List<ApiActivity>> fetchActivitiesFromAPI(FetchActivitiesFromAPIRef ref, String placeId) {
+  final repo = ref.read(apiActivitiesRepositoryProvider);
+  return repo.getActivitiesInPlace(placeId: placeId);
+}
