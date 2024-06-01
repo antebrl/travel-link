1f397bc7534be746015fa440f508c0b26bcb1c73
Ante Brähler <ante.braehler@stud.h-da.de>
2024-06-01 22:18:14 +0200

    refactor: extract auto-complete search

--- app/lib/src/features/activities/7_api_search_screen/search_screen.dart
+++ app/lib/src/features/activities/7_api_search_screen/search_screen.dart
@@ -1,19 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:http/http.dart' as http;
-import 'package:travel_link/src/common_widgets/calendar_popup_view.dart';
+import 'package:travel_link/src/common_widgets/auto_complete_search.dart';
 import 'package:travel_link/src/features/activities/3_activities_screen/domain/api_activity.dart';
 import 'package:travel_link/src/features/activities/3_activities_screen/presentation/activities_api_screen.dart';
-import 'package:travel_link/src/features/my_trips/data/my_trips_repository.dart';
-import 'package:travel_link/src/features/my_trips/domain/destination.dart';
-import 'package:travel_link/src/features/my_trips/presentation/my_trips_controller.dart';
-import 'package:travel_link/src/utils/constants/api_constants.dart';
 import 'package:travel_link/src/utils/constants/colors.dart';
-import 'package:travel_link/src/utils/logging/logger.dart';
 
 class SearchScreen extends ConsumerStatefulWidget {
   const SearchScreen({super.key});
@@ -23,37 +14,11 @@ class SearchScreen extends ConsumerStatefulWidget {
 }
 
 class _SearchScreenState extends ConsumerState<SearchScreen> {
-  String? _queryDestination;
-  late Iterable<Destination> _destinationSuggestions = <Destination>[];
-  Destination? _selectedDestination;
+  final DestinationController _controller = DestinationController();
   Set<String> _categoryList = {};
-
-  Future<Iterable<Destination>> getDestinationSuggestion(String input) async {
-    // TODO: Request with language code
-    final String request =
-        '${CustomApiConstants.autocompleteBaseURL}?type=city&text=$input&apiKey=${CustomApiConstants.geoapifySecretKey}&limit=3&format=json';
-    final response = await http.get(Uri.parse(request));
-
-    if (response.statusCode == 200) {
-      final destinations =
-          (json.decode(response.body)['results'] as List<dynamic>);
-
-      return destinations.map((destination) =>
-          Destination.fromMap(destination as Map<dynamic, dynamic>));
-    } else {
-      logger.e(
-        'Failed to load and parse destination suggestions',
-        error: response.body,
-      );
-      return [];
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    final state = ref.watch(myTripsControllerProvider);
-
     return SingleChildScrollView(
       child: Center(
         child: Padding(
@@ -124,122 +89,30 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                         ),
                       ),
                       const SizedBox(height: 10),
-                      Autocomplete<Destination>(
-                        optionsBuilder:
-                            (TextEditingValue textEditingValue) async {
-
-                          _queryDestination = textEditingValue.text;
-                          if (_queryDestination == '') {
-                            return const Iterable<Destination>.empty();
-                          }
-                          final Iterable<Destination> options =
-                              await getDestinationSuggestion(
-                            _queryDestination!,
-                          );
-
-                          if(_queryDestination == _selectedDestination?.formatted) 
-                            return [];
-                  
-                          // If the query has changed, don't update and wait for next options build
-                          if (_queryDestination != textEditingValue.text) {
-                            return _destinationSuggestions;
-                          }
-                          _destinationSuggestions = options;
-                          return options;
-                        },
-                        displayStringForOption: (option) => option.formatted,
-                        onSelected: (destination) {
-                          _selectedDestination = destination;
-                        },
-                        fieldViewBuilder: (
-                          BuildContext context,
-                          TextEditingController textEditingController,
-                          FocusNode focusNode,
-                          VoidCallback onFieldSubmitted,
-                        ) {
-                          return TextFormField(
-                            controller: textEditingController,
-                            focusNode: focusNode,
-                            onFieldSubmitted: (String value) {
-                              onFieldSubmitted();
-                            },
-                            decoration: InputDecoration(
-                              hintText:
-                                  'City...', // Nutze hintText statt labelText
-                              fillColor: CustomColors.white,
-                              filled: true,
-                              border: InputBorder.none,
-                  
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(14),
-                                borderSide: const BorderSide(
-                                  width: 1,
-                                  color: CustomColors.white,
-                                ),
-                              ),
-                              labelStyle: const TextStyle(
-                                color: CustomColors
-                                    .primary, // Farbe für den Labeltext
-                              ),
+                      AutoCompleteSearch(
+                        controller: _controller,
+                        horizontalPadding: 95,
+                        filterByType: 'city',
+                        textFieldDecoration: InputDecoration(
+                          hintText: 'City...',
+                          fillColor: CustomColors.white,
+                          filled: true,
+                          border: InputBorder.none,
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: const BorderSide(
+                              color: CustomColors.white,
                             ),
-                            style: const TextStyle(
-                              color: CustomColors
-                                  .primary, // Farbe für den Eingabetext
-                            ),
-                          );
-                        },
-                        optionsViewBuilder: (
-                          BuildContext context,
-                          AutocompleteOnSelected<Destination> onSelected,
-                          Iterable<Destination> options,
-                        ) {
-                          final List<Destination> items = options.toList();
-                          return Align(
-                            alignment: Alignment.topLeft,
-                            child: Material(
-                              color: CustomColors.primaryBackground,
-                              elevation: 4,
-                              borderRadius: BorderRadius.circular(10),
-                              child: SizedBox(
-                                height: 52.0 * items.length,
-                                width: MediaQuery.of(context).size.width - 95,
-                                child: ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: items.length,
-                                  itemBuilder: (BuildContext context, int index) {
-                                    final Destination option = items[index];
-                                    return Column(
-                                      children: [
-                                        ListTile(
-                                          leading: const Icon(
-                                            Icons.location_on,
-                                            color: CustomColors.primary,
-                                          ),
-                                          title: Text(
-                                            option.formatted,
-                                            style: textTheme.bodySmall?.copyWith(
-                                              color: CustomColors.black,
-                                            ),
-                                          ),
-                                          onTap: () {
-                                            onSelected(option);
-                                          },
-                                        ),
-                                        if (index != items.length - 1)
-                                          const Divider(
-                                            endIndent: 10,
-                                            indent: 10,
-                                            thickness: 1,
-                                            height: 1,
-                                          ),
-                                      ],
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                          );
-                        },
+                          ),
+                          labelStyle: const TextStyle(
+                            color:
+                                CustomColors.primary, // Farbe für den Labeltext
+                          ),
+                        ),
+                        textStyle: const TextStyle(
+                          color:
+                              CustomColors.primary, // Farbe für den Eingabetext
+                        ),
                       ),
                       const SizedBox(height: 15),
                       const Text(
@@ -289,11 +162,11 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                       const SizedBox(height: 15),
                       ElevatedButton(
                         onPressed: () {
-                          if (_selectedDestination != null) {
+                          if (_controller.selectedDestination != null) {
                             Navigator.of(context).push(
                               MaterialPageRoute<APIActivitiesScreen>(
                                 builder: (context) => APIActivitiesScreen(
-                                  destination: _selectedDestination!,
+                                  destination: _controller.selectedDestination!,
                                   categoryList: _categoryList,
                                 ),
                               ),
@@ -301,8 +174,8 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
                           }
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              CustomColors.white, // Hintergrundfarbe des Buttons
+                          backgroundColor: CustomColors
+                              .white, // Hintergrundfarbe des Buttons
                           side: BorderSide.none, // Keine Seitenlinie
                         ),
                         child: const Text(
