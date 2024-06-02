0af37c4a918e39debd5fd1bbcc7823ad62cc70d8
Ante Brähler <ante.braehler@stud.h-da.de>
2024-06-02 02:43:48 +0200

    feat: add destination label on image

--- app/lib/src/features/explore_trips/presentation/public_trip_card.dart
+++ app/lib/src/features/explore_trips/presentation/public_trip_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_image_stack/flutter_image_stack.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:travel_link/src/features/explore_trips/domain/trip.dart';
 import 'package:travel_link/src/routing/app_router.dart';
 
@@ -42,7 +43,8 @@ class PublicTripCard extends ConsumerWidget {
             child: Card(
               elevation: 10,
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
+                borderRadius: BorderRadius.circular(20),
+              ),
               child: Container(
                 width: 400,
                 padding: const EdgeInsets.all(12),
@@ -75,17 +77,17 @@ class PublicTripCard extends ConsumerWidget {
                               ),
                             ),
                             FlutterImageStack(
-                              imageList: [
+                              imageList: const [
                                 'https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80',
                                 'https://images.unsplash.com/photo-1612594305265-86300a9a5b5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
                                 'https://images.unsplash.com/photo-1612626256634-991e6e977fc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1712&q=80',
                                 'https://images.unsplash.com/photo-1593642702749-b7d2a804fbcf?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
                               ],
                               extraCountTextStyle: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                              showTotalCount: true,
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                               totalCount: 4,
                               itemRadius: 40,
                               itemCount:
@@ -108,20 +110,23 @@ class PublicTripCard extends ConsumerWidget {
                           TextSpan(
                             text: '„',
                             style: TextStyle(
-                                fontSize: 27,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue),
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
                           ),
                           TextSpan(
                             text:
                                 'Eine spannende Reise nach Japan oder. Tretet bei 👺🗾🏯🍣',
                           ),
                           TextSpan(
-                              text: '”',
-                              style: TextStyle(
-                                  fontSize: 27,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue)),
+                            text: '”',
+                            style: TextStyle(
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
                         ],
                       ),
                       maxLines: 4,
@@ -133,6 +138,7 @@ class PublicTripCard extends ConsumerWidget {
                       child: Container(
                         height: 375,
                         width: 300,
+                        alignment: Alignment.bottomLeft,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(40),
                           boxShadow: [
@@ -145,8 +151,36 @@ class PublicTripCard extends ConsumerWidget {
                           ],
                           image: const DecorationImage(
                             image: NetworkImage(
-                                'https://media.istockphoto.com/id/1998131648/de/foto/altstadt-von-burghausen-mit-burgberg.jpg?s=1024x1024&w=is&k=20&c=nvlz0e9DkNmf4_84ahASlYZVnGN-7NTKm9L3zppAOZI='),
+                              'https://media.istockphoto.com/id/1998131648/de/foto/altstadt-von-burghausen-mit-burgberg.jpg?s=1024x1024&w=is&k=20&c=nvlz0e9DkNmf4_84ahASlYZVnGN-7NTKm9L3zppAOZI=',
+                            ),
                             fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 25,
+                          ),
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF4D5652).withOpacity(0.8),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 8,
+                                horizontal: 14,
+                              ),
+                              child: Text(
+                                trip.destination.formatted,
+                                style: GoogleFonts.adamina(
+                                  color: Colors.white, // Textfarbe
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
                           ),
                         ),
                       ),
