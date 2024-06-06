955e9b8a9f993f190dc1235c2142b58a10ad1275
DawidDeveloper <dawid.grabowski@stud.h-da.de>
2024-06-06 15:34:03 +0200

    Feat: Add inkewell

--- app/lib/src/features/activities/1_activities_start_screen/main_screen.dart
+++ app/lib/src/features/activities/1_activities_start_screen/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_link/src/features/activities/1_activities_start_screen/presentation/curved_edges.dart';
+import 'package:travel_link/src/features/activities/2_continents_screen/presentation/activities_continents_screen.dart';
 import 'package:travel_link/src/utils/constants/colors.dart';
 
 class ActivitiesMainScreen extends StatelessWidget {
@@ -48,6 +49,26 @@ class ActivitiesMainScreen extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          const SizedBox(height: 10),
+          InkWell(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                color: CustomColors.primary,
+                child: Text('Most popular activities'),
+              ),
+            ),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      //APIActivitiesScreen(),
+                      const ActivitiesContinentsScreen(),
+                ),
+              );
+            },
           ),
           const SizedBox(height: 10),
           Text(
