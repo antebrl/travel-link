e3061b09115bb8808b285adf0b2d86f573d406a9
DawidDeveloper <dawid.grabowski@stud.h-da.de>
2024-06-07 16:37:13 +0200

    feat: Changes to Main Activity Screen + 2 new widgets to display activities in different way

--- app/lib/src/features/activities/1_activities_start_screen/main_screen.dart
+++ app/lib/src/features/activities/1_activities_start_screen/main_screen.dart
@@ -5,7 +5,10 @@ import 'package:travel_link/src/features/activities/1_activities_start_screen/pr
 import 'package:travel_link/src/features/activities/2_continents_screen/domain/continent.dart';
 import 'package:travel_link/src/features/activities/2_continents_screen/presentation/activities_continents_screen.dart';
 import 'package:travel_link/src/features/activities/3_activities_screen/data/activity_data.dart';
+import 'package:travel_link/src/features/activities/3_activities_screen/data/popular_activity_data.dart';
 import 'package:travel_link/src/features/activities/3_activities_screen/domain/api_activity.dart';
+import 'package:travel_link/src/features/activities/3_activities_screen/presentation/items/horizontal_activity_item.dart';
+import 'package:travel_link/src/features/activities/3_activities_screen/presentation/items/small_activity_item.dart';
 import 'package:travel_link/src/utils/constants/colors.dart';
 
 class ActivitiesMainScreen extends StatefulWidget {
@@ -18,6 +21,25 @@ class ActivitiesMainScreen extends StatefulWidget {
 class _ActivitiesMainScreenState extends State<ActivitiesMainScreen> {
   ContinentType selectedContinent = ContinentType.none;
   List<ApiActivity> selectedActivities = [];
+  String getContinentDisplayName(ContinentType continent) {
+    switch (continent) {
+      case ContinentType.northAmerica:
+        return 'North America';
+      case ContinentType.southAmerica:
+        return 'South America';
+      case ContinentType.africa:
+        return 'Africa';
+      case ContinentType.europe:
+        return 'Europe';
+      case ContinentType.asia:
+        return 'Asia';
+      case ContinentType.australia:
+        return 'Australia';
+      case ContinentType.none:
+      default:
+        return '';
+    }
+  }
 
   @override
   void initState() {
@@ -52,7 +74,7 @@ class _ActivitiesMainScreenState extends State<ActivitiesMainScreen> {
             clipper: CustomCurvedEdges(),
             child: Container(
               color: CustomColors.primary,
-              padding: const EdgeInsets.all(0),
+              padding: EdgeInsets.zero,
               child: SizedBox(
                 height: 200,
                 child: Stack(
@@ -95,78 +117,110 @@ class _ActivitiesMainScreenState extends State<ActivitiesMainScreen> {
                 ),
           ),
           const SizedBox(height: 10),
-          InkWell(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.only(
-                  left: 40, top: 5, bottom: 15, right: 40),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: CustomColors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                    offset: const Offset(0, 3),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 40, top: 5, bottom: 15, right: 40),
+            child: InkWell(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: CustomColors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
                   ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-                child: Image.asset(
-                  'images/continents/ContinentsMap.png',
-                  height: 150,
-                  width: double.infinity,
-                  fit: BoxFit.fitHeight,
+                  child: Image.asset(
+                    'assets/images/continents/ContinentsMap.png',
+                    height: 150,
+                    width: double.infinity,
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
               ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        const ActivitiesContinentsScreen(),
+                  ),
+                );
+              },
             ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) =>
-                      //APIActivitiesScreen(),
-                      const ActivitiesContinentsScreen(),
-                ),
-              );
-            },
           ),
           const SizedBox(height: 10),
           Column(
             children: [
               Text(
-                'Explore some Activities in ${selectedContinent.toString().split('.').last} :',
+                'Explore some Activities in ${getContinentDisplayName(selectedContinent)}:',
                 style: Theme.of(context).textTheme.headlineSmall!.copyWith(
                       color: CustomColors.primary,
                     ),
               ),
-              ListView.builder(
-                itemCount: selectedActivities.length,
-                itemBuilder: (context, index) {
-                  final activity = selectedActivities[index];
-                  return ListTile(
-                    title: Text(activity.name),
-                    subtitle: Text(activity.location.formatted),
-                  );
-                },
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 30, top: 5, bottom: 15, right: 30),
+                child: SizedBox(
+                  height: 200, // Höhe des horizontalen ListViews
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: selectedActivities.length,
+                    itemBuilder: (context, index) {
+                      final activity = selectedActivities[index];
+                      return SmallActivityItem(
+                        activity: activity,
+                        key: UniqueKey(),
+                      );
+                    },
+                  ),
+                ),
               ),
             ],
           ),
           const SizedBox(height: 10),
-
-          //
-
-          const SizedBox(height: 10),
-          Text(
-            'Explore the most popular Activities: ',
-            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                  color: CustomColors.primary,
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 30, top: 5, bottom: 15, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Text(
+                  'Explore the most popular Activities: ',
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: CustomColors.primary,
+                      ),
+                  textAlign: TextAlign.center,
                 ),
-          )
+                const SizedBox(height: 10),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: popularActivityData.length,
+                  itemBuilder: (context, index) {
+                    final activity = popularActivityData[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: HorizontalActivityItem(
+                        activity: activity,
+                        key: UniqueKey(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ app/lib/src/features/activities/3_activities_screen/presentation/items/horizontal_activity_item.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:travel_link/src/features/activities/3_activities_screen/domain/api_activity.dart';
+import 'package:travel_link/src/features/activities/5_activities_details_screen/api_activities_details_screen.dart';
+import 'package:travel_link/src/utils/constants/colors.dart';
+import 'package:travel_link/src/utils/theme/widget_themes/text_theme.dart';
+
+class HorizontalActivityItem extends StatefulWidget {
+  const HorizontalActivityItem({required this.activity, Key? key})
+      : super(key: key);
+  final ApiActivity activity;
+
+  @override
+  _HorizontalActivityItemState createState() => _HorizontalActivityItemState();
+}
+
+class _HorizontalActivityItemState extends State<HorizontalActivityItem> {
+  late Future<String> _imageFuture;
+  static final Map<String, String> _imageCache = {};
+
+  @override
+  void initState() {
+    super.initState();
+    if (_imageCache.containsKey(widget.activity.name)) {
+      final imageName = _imageCache[widget.activity.name];
+      if (imageName != null) {
+        setState(() {
+          widget.activity.imagePaths[0] = imageName;
+        });
+      }
+      _imageFuture = Future.value(imageName);
+    } else {
+      _imageFuture = fetchImage(widget.activity.name);
+    }
+  }
+
+  Future<String> fetchImage(String activityName) async {
+    final formattedName = activityName.replaceAll(' ', '_');
+    final formattedLink =
+        'https://en.wikipedia.org/w/api.php?action=query&titles=$formattedName&prop=pageimages&format=json&pithumbsize=1000';
+    final response = await http.get(Uri.parse(formattedLink));
+    final Map<String, dynamic> data =
+        json.decode(response.body) as Map<String, dynamic>;
+    final pages = data['query']['pages'];
+    final pageId = pages.keys.first;
+    final imageUrl = pages[pageId]['thumbnail']['source'] as String;
+
+    _imageCache[activityName] = imageUrl;
+
+    setState(() {
+      widget.activity.imagePaths[0] = imageUrl;
+    });
+
+    return imageUrl;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute<ApiActivitiesDetailsScreen>(
+            builder: (BuildContext context) =>
+                ApiActivitiesDetailsScreen(activity: widget.activity),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: CustomColors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+              ),
+              child: FutureBuilder<String>(
+                future: _imageFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: CircularProgressIndicator()),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Center(child: Icon(Icons.error)),
+                    );
+                  } else {
+                    return Image.network(
+                      snapshot.data!,
+                      height: 100,
+                      width: 120,
+                      fit: BoxFit.cover,
+                    );
+                  }
+                },
+              ),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 8),
+                  Text(
+                    widget.activity.name,
+                    style: CustomTextTheme.lightTextTheme.bodyMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: CustomColors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    '${widget.activity.location.country}${widget.activity.location.city.isNotEmpty ? ' · ${widget.activity.location.city}' : ''}',
+                    style: CustomTextTheme.lightTextTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Visitiors last year: ${widget.activity.amountVisitors}',
+                    style: CustomTextTheme.lightTextTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
