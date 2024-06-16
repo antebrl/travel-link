3cca612bd230e2fa3a7973fe3334bfd2b6404587
Ante Brähler <ante.braehler@stud.h-da.de>
2024-06-16 16:04:47 +0000

    Resolve "Feat: Leave Trip" (Closes #74)
    * feat: leave trip functionality
    
    * fix: explore trip card overflowing
    
    * fix: resize chat message tiles

--- app/lib/src/features/trip_overview/presentation/trip_overview_screen.dart
+++ app/lib/src/features/trip_overview/presentation/trip_overview_screen.dart
@@ -73,7 +73,7 @@ class _TripOverviewScreenState extends ConsumerState<TripOverviewScreen>
                           ],
                         ),
                         onTap: () {
-                          showDialog<AlertDialog>(
+                          showDialog(
                             context: context,
                             builder: (BuildContext context) {
                               return AlertDialog(
@@ -92,11 +92,13 @@ class _TripOverviewScreenState extends ConsumerState<TripOverviewScreen>
                                     child: const Text('Leave'),
                                     onPressed: () async {
                                       // Perform leave trip logic
-                                      await ref.read(myTripsControllerProvider.notifier).leaveTrip(trip: trip);
+                                      await ref
+                                          .read(myTripsControllerProvider
+                                              .notifier)
+                                          .leaveTrip(trip: trip);
                                       ref.invalidate(fetchMyTripsProvider);
 
-                                      if(mounted)
-                                      Navigator.of(context).pop();
+                                      if (mounted) Navigator.of(context).pop();
                                     },
                                   ),
                                 ],
