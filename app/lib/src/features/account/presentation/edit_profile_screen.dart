fefcc70a5cc1c2dc5fe9b2cd7e09a91e48039595
TwitchBluelow <fabriziomonaco@outlook.de>
2024-06-18 19:17:07 +0200

    feat: init edit_profile_screen

---
+++ app/lib/src/features/account/presentation/edit_profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
