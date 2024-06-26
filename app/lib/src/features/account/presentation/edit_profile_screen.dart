912dd0397156b5d509f2f8ac1430d54cd573f031
stfamonac <fabrizio.monaco@stud.h-da.de>
2024-06-26 12:37:54 +0200

    feat: extended edit profile screen

--- app/lib/src/features/account/presentation/edit_profile_screen.dart
+++ app/lib/src/features/account/presentation/edit_profile_screen.dart
@@ -1,11 +1,117 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:travel_link/src/common_widgets/boxed_content.dart';
+import 'package:travel_link/src/utils/theme/widget_themes/boxDecoration_theme.dart';
 
 class EditProfileScreen extends StatelessWidget {
   const EditProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Center(
+            child: SizedBox(
+              height: 100,
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: CachedNetworkImage(
+                    imageUrl: 'https://via.placeholder.com/150',
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          BoxedContent(
+            boxDecoration: boxDecoration,
+            content: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Personal Information",
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      labelStyle: TextStyle(fontSize: 15),
+                      hintStyle: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    maxLines: 3,
+                    maxLength: 200,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    decoration: const InputDecoration(
+                      labelText: 'Description',
+                      labelStyle: TextStyle(fontSize: 15),
+                      hintStyle: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          BoxedContent(
+            boxDecoration: boxDecoration,
+            content: Padding(
+              padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Languages",
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                    child: GridView.count(
+                      crossAxisCount: 5, // Number of columns in the grid
+                      children: List.generate(50, (index) {
+                        // Change the number to generate more or fewer chips
+                        return Center(
+                          child: Chip(
+                            label: Text('Chip $index'),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
