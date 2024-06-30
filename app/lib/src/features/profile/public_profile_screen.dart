4d642ba40d79a6170be0ffd3a3f59d6b901d4d3e
TwitchBluelow <fabriziomonaco@outlook.de>
2024-06-30 14:23:55 +0200

    feat: complete add to trip button on public profile

--- app/lib/src/features/profile/public_profile_screen.dart
+++ app/lib/src/features/profile/public_profile_screen.dart
@@ -4,9 +4,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:travel_link/src/common_widgets/boxed_content.dart';
 import 'package:travel_link/src/features/account/data/account_repository.dart';
+import 'package:travel_link/src/features/my_trips/data/my_trips_repository.dart';
+import 'package:travel_link/src/features/my_trips/presentation/my_trips_controller.dart';
 import 'package:travel_link/src/features/trip_overview/data/user_repository.dart';
 import 'package:travel_link/src/utils/constants/colors.dart';
 import 'package:travel_link/src/utils/constants/image_strings.dart';
+import 'package:travel_link/src/utils/helpers/localization.dart';
 import 'package:travel_link/src/utils/theme/widget_themes/boxDecoration_theme.dart';
 
 import '../../common_widgets/profile_widgets.dart';
@@ -106,7 +109,108 @@ class _ProfileScreenState extends ConsumerState<UserProfileScreen> {
                           ),
                         ),
                         IconButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            var myTrips = ref.watch(fetchMyTripsProvider);
+                            showModalBottomSheet<void>(
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (BuildContext context) =>
+                                  SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Add to trip",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headlineSmall!
+                                          .copyWith(
+                                            color: CustomColors.primary,
+                                            fontSize: 20,
+                                          ),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    myTrips.when(
+                                      data: (trips) {
+                                        trips
+                                            .where((trip) => trip.participants
+                                                .contains(widget.targetuid))
+                                            .toList()
+                                            .forEach((trip) {
+                                          trips.remove(trip);
+                                        });
+                                        if (trips.isEmpty) {
+                                          return Center(
+                                            child: Text(
+                                              "No trips available",
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodySmall!
+                                                  .copyWith(
+                                                    color: CustomColors.primary,
+                                                    fontSize: 20,
+                                                  ),
+                                            ),
+                                          );
+                                        }
+                                        return ListView.builder(
+                                          shrinkWrap: true,
+                                          physics:
+                                              const BouncingScrollPhysics(),
+                                          itemCount: trips.length,
+                                          itemBuilder: (context, index) {
+                                            return ListTile(
+                                              title: Text(trips[index].name),
+                                              leading: CircleAvatar(
+                                                radius: 17,
+                                                backgroundImage: NetworkImage(
+                                                  trips[index].images.isNotEmpty
+                                                      ? trips[index].images[0]
+                                                      : CustomImages
+                                                          .tripDestinationImagePlaceholderUrl,
+                                                ),
+                                              ),
+                                              onTap: () {
+                                                // Handle trip selection
+                                                final mytripsController =
+                                                    ref.read(
+                                                        myTripsControllerProvider
+                                                            .notifier);
+                                                // ignore: cascade_invocations
+                                                mytripsController.addToTrip(
+                                                    trip: trips[index],
+                                                    uid: widget.targetuid);
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  SnackBar(
+                                                    content: Center(
+                                                      child: Text(context.loc
+                                                          .successfullyAdded),
+                                                    ),
+                                                    duration: const Duration(
+                                                        seconds: 1,
+                                                        milliseconds: 400),
+                                                  ),
+                                                );
+                                              },
+                                            );
+                                          },
+                                        );
+                                      },
+                                      loading: () => const Center(
+                                        child: CircularProgressIndicator(),
+                                      ),
+                                      error: (error, stackTrace) {
+                                        return Center(
+                                          child:
+                                              Text(context.loc.loginToSeeTrips),
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
                           icon: const Icon(
                             Icons.group_add,
                             color: CustomColors.buttonPrimary,
