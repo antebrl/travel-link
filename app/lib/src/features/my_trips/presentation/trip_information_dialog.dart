c044481cf62cb5b1bc2f3bb314c66fc4603da3f7
Ante Brähler <ante.braehler@stud.h-da.de>
2024-07-02 00:17:50 +0200

    feat: translate explore trips

--- app/lib/src/features/my_trips/presentation/trip_information_dialog.dart
+++ app/lib/src/features/my_trips/presentation/trip_information_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:travel_link/src/utils/constants/colors.dart';
 import 'package:travel_link/src/utils/helpers/localization.dart';
 
