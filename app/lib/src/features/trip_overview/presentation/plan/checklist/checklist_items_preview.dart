656d20735b27f6635a5b10f1d24b3acca2ce365d
sttokoeni <tom.koeniger@stud.h-da.de>
2024-07-02 13:52:32 +0200

    Feat: Added Group Checklist preview if user is not logged in

--- app/lib/src/features/trip_overview/presentation/plan/checklist/checklist_items_preview.dart
+++ app/lib/src/features/trip_overview/presentation/plan/checklist/checklist_items_preview.dart
@@ -24,22 +24,45 @@ class ChecklistPreview extends ConsumerWidget {
     final currentUser = ref.watch(firebaseAuthProvider).currentUser;
 
     if (currentUser == null) {
-      return const Center(
-        child: Text('You need to log in to view the checklist'),
+      final groupChecklistProvider = ref.watch(fetchTripChecklistProvider(tripId: tripId, onlyPublic: true));
+      return groupChecklistProvider.when(
+        data: (checklist) {
+          final groupChecklist = checklist.take(3).toList();
+          return ListView.builder(
+            itemCount: groupChecklist.length,
+            itemBuilder: (context, index) {
+              final item = groupChecklist[index];
+              final completedCount = item.asigneesCompleted.where((completed) => completed).length;
+              final totalCount = item.asigneesCompleted.length;
+              return ListTile(
+                title: Text(item.title),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (item.dueDate != null) Text('Due: ${DateFormat('dd/MM/yyyy').format(item.dueDate!)}'),
+                    Text('Completed: $completedCount/$totalCount'),
+                  ],
+                ),
+              );
+            },
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => const Center(child: Text('Error loading checklist')),
       );
     }
 
-//Ensure that only checklist items are displayed that are unchecked in personal checklist
+    // Ensure that only checklist items are displayed that are unchecked in personal checklist
     return checklistProvider.when(
       data: (checklist) {
-
-        if(currentUser != null) {
+        if (currentUser != null) {
           checklist = checklist.where((item) {
-          final userIndex = item.asignees.indexOf(currentUser.uid);
-          return userIndex != -1 && !item.asigneesCompleted[userIndex];
-        }).toList();
+            final userIndex = item.asignees.indexOf(currentUser.uid);
+            return userIndex != -1 && !item.asigneesCompleted[userIndex];
+          }).toList();
         }
-
 
         return ListView.builder(
           itemCount: checklist.length > maxItems ? maxItems : checklist.length,
